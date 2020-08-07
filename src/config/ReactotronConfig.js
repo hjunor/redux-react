fc8912c907123db71be972d5b0ab88ef83ec9c1e
hjunor <hjunor@gmail.com>
2020-08-07 09:59:04 -0300

    add: reactotrom

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,12 @@
+import Reactotrom from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reactotrom.configure()
+    .use(reactotronRedux())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
